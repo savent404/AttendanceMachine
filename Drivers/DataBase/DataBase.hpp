66ea8ea761afa21ca95af7b75e6c6a866a14d6f8
Unknown <savent_gate@outlook.com>
2017-12-27 11:24:16 +0800

    为数据结果添加注释

--- Drivers/DataBase/DataBase.hpp
+++ Drivers/DataBase/DataBase.hpp
@@ -6,6 +6,12 @@
 
 class DB_Base {
 public:
+    /**
+     * @brief 创建一个基础数据类型
+     * @param data 指向需要初始化数据的内容
+     * @param size 数据大小
+     * @param type 数据类型
+     */
     DB_Base(const uint8_t* data, const size_t size, const uint32_t type)
     {
         _size = size;
@@ -36,6 +42,9 @@ public:
     {
         return _type;
     }
+    /**
+     * @brief  重写数据内容
+     */
     inline void overWrite(const uint8_t* data) const
     {
         memcpy(_data, data, _size);
@@ -46,6 +55,10 @@ public:
             return;
         memcpy(_data, data.data(), _size);
     }
+    /**
+     * @brief  判断数据有效性
+     * @retvl  如果无效返回true
+     */
     bool isNull() const
     {
         const uint8_t* ptr = _data;
@@ -55,6 +68,11 @@ public:
         }
         return true;
     }
+
+    /**
+     * @brief  重载== 比较两个Base是否相同
+     * @note   不光比较数据内容, 数据类型以及是否是有效数据也会检查
+     */
     bool operator==(const DB_Base& other) const
     {
         if (type() != other.type() || size() != other.size() || other.isNull()) {
@@ -70,6 +88,10 @@ public:
         }
         return true;
     }
+
+    /**
+     * @brief  重载== 只比较数据内容的版本
+     */
     bool operator==(const uint8_t* addr) const
     {
         const uint8_t* aptr = data();
@@ -88,6 +110,9 @@ private:
     uint32_t _type;
 };
 
+/**
+ * @brief  指纹数据占2Bytes,数据类型为@1
+ */
 class DB_FingerPrint : public DB_Base {
 public:
     DB_FingerPrint(const uint8_t* data = "00")
@@ -95,6 +120,10 @@ public:
     {
     }
 };
+
+/**
+ * @brief  RFID数据占4字节，数据类型为@2
+ */
 class DB_RFID : public DB_Base {
 public:
     DB_RFID(const uint8_t* data = "0000")
@@ -102,6 +131,11 @@ public:
     {
     }
 };
+
+/**
+ * @brief  房间ID数据占4字节，数据类型为@3
+ * @note   数据以ASCII方式保存
+ */
 class DB_RoomID : public DB_Base {
 public:
     DB_RoomID(const uint8_t* data = "0000")
@@ -109,6 +143,11 @@ public:
     {
     }
 };
+
+/**
+ * @brief  密码数据占6字节，数据类型为@4
+ * @note   密码以明文保存，为提供额外保护需自行添加convert函数进行加密
+ */
 class DB_Password : public DB_Base {
 public:
     DB_Password(const uint8_t* data = "000000")
@@ -122,6 +161,9 @@ public:
     DB_Usr()
     {
     }
+    /**
+     * @brief  通过打包好数据的数据指针初始化用户数据
+     */
     DB_Usr(const uint8_t* addr)
     {
         const uint8_t* ptr = addr;
@@ -153,6 +195,10 @@ public:
         rid.overWrite(r);
         pid.overWrite(p);
     }
+
+    /**
+     * @brief  通过打包好的数据的数据指针重写用户数据
+     */
     void overWrite(const uint8_t* data) const
     {
         const uint8_t* ptr = data;
@@ -177,6 +223,10 @@ public:
         cnt += pid.size();
         return cnt;
     }
+
+    /**
+     * @brief  将用户数据打包到数据指针地址中
+     */
     void packet(uint8_t* addr) const
     {
         uint8_t* ptr = addr;
@@ -190,6 +240,10 @@ public:
         ptr += rid.size();
         memcpy(ptr, pid.data(), pid.size());
     }
+
+    /**
+     * @brief  比较一个数据基类，查看该用户是否拥有相同数据项
+     */
     bool isThere(const DB_Base& base) const
     {
         for (int i = 0; i < 5; i++) {
@@ -208,6 +262,10 @@ public:
         }
         return false;
     }
+
+    /**
+     * @brief  判断该用户是否为无效用户(数据都无效)
+     */
     bool isAllNull() const
     {
         for (int i = 0; i < 5; i++) {

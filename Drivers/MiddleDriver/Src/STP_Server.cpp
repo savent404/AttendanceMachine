ff586304a0265925d2618c07cb76f4aeae902ae2
savent liao <savent_gate@outlook.com>
2017-12-28 11:11:30 +0800

    更新server函数

--- Drivers/MiddleDriver/Src/STP_Server.cpp
+++ Drivers/MiddleDriver/Src/STP_Server.cpp
@@ -19,19 +19,19 @@ void STP_ServerCallback()
 /** Class : Base ********************************************************/
 STP_ServerBase::STP_ServerBase() {}
 STP_ServerBase::~STP_ServerBase() {}
-bool STP_ServerBase::sendMessage(const uint8_t* message, size_t size)
+bool STP_ServerBase::sendMessage(enum CMD cmd, const uint8_t* message, size_t size)
 {
     uint8_t ans = 0;
-    uint8_t* pt = (uint8_t*)malloc(size + 4);
-    const uint8_t start_frame[2] = { 0xA5, 0x5A };
-    memcpy(pt, start_frame, 2);
-    memcpy(pt + 2, &size, 1);
-    memcpy(pt + 3, message, size);
+    uint8_t buffer[8];
+    buffer[0] = START_FRAME;
+    buffer[1] = (uint8_t)cmd;
+    buffer[2] = (uint8_t)size;
+    buffer[3 + size] = ans;
     for (int i = 0; i < size; i++) {
-        ans += message[i];
+        buffer[3 + i] = *message;
+        ans += *message++;
     }
-    memcpy(pt + 3 + size, &ans, 1);
-    return send(pt, size + 4);
+    return send(buffer, size + 4);
 }
 bool STP_ServerBase::reciMessage()
 {
@@ -63,12 +63,19 @@ bool STP_ServerRS485::setReminder()
 void STP_ServerRS485::Callback()
 {
     // 强制的起始帧检查，以防止发送方信息结构错误后不能正常接收下一帧
-    if (frameBuffer[0] == 0xA5 && recBuffer[0] == 0x5A) {
+    if (*recBuffer == START_FRAME) {
+        Recive_Status = waitCMD;
+    } else if (Recive_Status == waitCMD) {
+        CMDBuffer[0] = recBuffer[0];
         Recive_Status = waitSize;
     } else if (Recive_Status == waitSize) {
         sizeBuffer[0] = recBuffer[0];
-        Recive_Status = waitMessage;
         dataPos = 0;
+        if (sizeBuffer[0] != 0) {
+            Recive_Status = waitMessage;
+        } else {
+            Recive_Status = waitCheck;
+        }
     } else if (Recive_Status == waitMessage) {
         messageBuffer[dataPos] = recBuffer[0];
         if (++dataPos >= sizeBuffer[0]) {
@@ -91,8 +98,6 @@ void STP_ServerRS485::Callback()
 GOT_A_MESSAGE:
 
 NORMAL_OPERA:
-    // 将当前收到的数据填入帧头检查缓存
-    frameBuffer[0] = recBuffer[0];
     // 等待下一个字符
     setReminder();
 }

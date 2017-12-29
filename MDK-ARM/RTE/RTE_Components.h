63cab9d8889a4a60bc4273faf7d334ea3e57f49b
Unknown <savent_gate@outlook.com>
2017-12-29 10:34:02 +0800

    Auto stash before merge of "master" and "origin/master"

---
+++ MDK-ARM/RTE/RTE_Components.h
@@ -0,0 +1,14 @@
+
+/*
+ * Auto generated Run-Time-Environment Component Configuration File
+ *      *** Do not modify ! ***
+ *
+ * Project: 'STP' 
+ * Target:  'STP' 
+ */
+
+#ifndef RTE_COMPONENTS_H
+#define RTE_COMPONENTS_H
+
+
+#endif /* RTE_COMPONENTS_H */
